140c768d3073c66c4d4dfb17a454571f8baaca33
alisaitbilgi <alisaitbilgi@gmail.com>
2017-03-03 10:55:07 +0300

    test coverage is increased

--- src/actions/setResponse.js
+++ src/actions/setResponse.js
@@ -1,16 +1,12 @@
 import {Request} from "ez-fetch-ajax";
 
-export const setResponse = (username = "nzakas") => {
+export const setResponse = (repos, users) => {
   return (dispatch) => {
     let lastWeek = new Date();
     let values;
     lastWeek.setDate((lastWeek.getDate()) - 7);
-    let url = {
-      repos: `https://api.github.com/users/${username}/repos`,
-      users: `https://api.github.com/users/${username}`
-    };
 
-    return Promise.all([Request.get(url.repos), Request.get(url.users)])
+    return Promise.all([Request.get(repos), Request.get(users)])
       .then(res => {
         if (Array.isArray(JSON.parse(res[0][1]))) {
           values = JSON.parse(res[0][1]).map(
